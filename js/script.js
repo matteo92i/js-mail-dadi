b6257f5c4a811443591e9038b9b392ee3430566e
Matteo92i <matteo.iovane@gmail.com>
2021-06-01 08:28:38 +0200

    listaemail

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var inserisciEmail = prompt('inserisci qui la ta email');
+var emailInserite = ['matteo.iovane','java.java','script.script','pokemon@pokemon'];
+
+var inserisciEmail = emailInserite.length
