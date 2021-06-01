608c40760b93795bcf532f6613faf6ff994f2582
Matteo92i <matteo.iovane@gmail.com>
2021-06-01 09:59:49 +0200

    inizio dado

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-var inserisciEmail = prompt('inserisci qui la ta email');
 var emailInserite = ['matteo.iovane','java.java','script.script','pokemon@pokemon'];    
+var inserisciEmail = prompt('inserisci qui la ta email');
 
-var inserisciEmail = emailInserite 
+var check = false
+
+for (var i = 0; i < emailInserite.length;)
 
 
